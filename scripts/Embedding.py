15103eb859c379c1b4acd79cb013c13271c55b05
TamVu02 <104774591+TamVu02@users.noreply.github.com>
2024-03-13 09:03:01 +0700

    Update Embedding.py

--- scripts/Embedding.py
+++ scripts/Embedding.py
@@ -89,6 +89,7 @@ class Embedding(nn.Module):
             optimizer_W.step()
             pbar.set_description('Embedding: Loss: {:.3f}, L2 loss: {:.3f}, Perceptual loss: {:.3f}, P-norm loss: {:.3f}'\
                 .format(loss, loss_dic['l2'], loss_dic['percep'], loss_dic['p-norm']))
+            print(gen_im.shape)
         return latent_in
 
     def invert_image_in_FS(self, image_path=None):
@@ -118,6 +119,7 @@ class Embedding(nn.Module):
             pbar.set_description(
                 'Embedding: Loss: {:.3f}, L2 loss: {:.3f}, Perceptual loss: {:.3f}, P-norm loss: {:.3f}'
                 .format(loss, loss_dic['l2'], loss_dic['percep'], loss_dic['p-norm']))
+            print(gen_im.shape)
         return latent_in, latent_F
 
 
