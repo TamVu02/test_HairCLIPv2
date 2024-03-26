b958198a395e0a3afd2ebcc8b289a9b336b9bd28
TamVu02 <104774591+TamVu02@users.noreply.github.com>
2024-03-26 15:00:52 +0700

    Add files via upload

--- scripts/feat_blend_2.py
+++ scripts/feat_blend_2.py
@@ -3,7 +3,6 @@ from utils.image_utils import dliate_erode
 import torch.nn.functional as F
 from PIL import Image
 from torchvision import transforms
-from scripts.text_proxy import TextProxy
 from utils.common import tensor2im
 from utils.inference_utils import get_average_image
 
@@ -31,6 +30,36 @@ def hairstyle_feature_blending_2(generator, seg, src_image, visual_mask, latent_
         global_hair_mask = torch.where(global_proxy_seg==10, torch.ones_like(global_proxy_seg), torch.zeros_like(global_proxy_seg))
         global_hair_mask_down = F.interpolate(global_hair_mask.float(), size=(1024, 1024), mode='bicubic')
         src_image = global_feature * global_hair_mask_down + src_image * (1-global_hair_mask_down)
+
+    feat_out_img = tensor2im(src_image[-1])
+    out = img_transforms(feat_out_img).unsqueeze(0).to('cuda')
+
+    with torch.no_grad():
+            avg_image = get_average_image(generator)
+            avg_image = avg_image.unsqueeze(0).repeat(out.shape[0], 1, 1, 1)
+            x_input = torch.cat([out, avg_image], dim=1)
+            img_gen_blend,blend_latent = generator(x_input,latent=None, return_latents=True, resize=False)
+    return feat_out_img, img_gen_blend, blend_latent
+
+def hair_aligning(generator, seg, src_image, visual_mask, latent_global=None):
+
+    if latent_global is not None:
+        #Seg on source
+        source_seg = torch.argmax(seg(src_image)[1], dim=1).unsqueeze(1).long()
+        bg = torch.where(source_seg == 0, torch.zeros_like(source_seg), torch.ones_like(source_seg)) #back ground mask
+        hair_mask_source_seg = torch.where(source_seg == 10, torch.ones_like(source_seg), torch.zeros_like(source_seg)) #source hair mask
+        without_hair_source_seg = torch.where(source_seg == 10, torch.zeros_like(source_seg), source_seg) #source mask remove hair
+        #Seg on ref after align
+        ref_aligned = generator.decoder.synthesis(latent_global, noise_mode='const')
+        ref_seg = torch.argmax(seg(ref_aligned)[1], dim=1).unsqueeze(1).long()
+        hair_mask_ref = torch.where(ref_seg == 10, torch.ones_like(ref_seg), torch.zeros_like(ref_seg)) #ref aligned hair mask
+        #New target: ref hair on source image without hair mask
+        new_target_seg = torch.where(ref_seg == 10, 10 * torch.ones_like(without_hair_source_seg), source_seg)
+        new_target_mean_seg = torch.where((new_target_seg == 0) * (ref_seg != 0), ref_seg, new_target_seg)
+        hair_mask_target = torch.where(new_target_mean_seg == 10, torch.ones_like(new_target_mean_seg), torch.zeros_like(new_target_mean_seg)) #hair mask on new target
+        hair_mask_target = F.interpolate(hair_mask_target.float().unsqueeze(0), size=(1024,1024), mode='bicubic')
+        
+        src_image = ref_aligned * hair_mask_target + src_image * (1-hair_mask_target)
 
     feat_out_img = tensor2im(src_image[-1])
     out = img_transforms(feat_out_img).unsqueeze(0).to('cuda')
--- scripts/refine_image.py
+++ scripts/refine_image.py
@@ -6,6 +6,7 @@ from utils.image_utils import process_display_input
 import torch.nn.functional as F
 from tqdm import tqdm
 from criteria.transfer_loss import TransferLossBuilder
+from criteria import masked_lpips
 from torchvision import transforms
 
 class RefineProxy(torch.nn.Module):
@@ -19,6 +20,10 @@ class RefineProxy(torch.nn.Module):
         self.delta_loss = torch.nn.MSELoss()
         self.landmark_loss = torch.nn.MSELoss()
         self.kp_extractor = self.load_kp_extractor()
+        self.percept_with_mask = masked_lpips.PerceptualLoss(
+                model="net-lin", net="vgg", vgg_blocks=['1', '2', '3'], use_gpu=self.opts.device == 'cuda'
+            )
+        self.percept_with_mask.eval()
 
     def weighted_ce_loss(self):
         weight_tmp = torch.zeros(16).cuda()
@@ -38,8 +43,6 @@ class RefineProxy(torch.nn.Module):
         return self.kp_extractor.face_alignment_net(((F.interpolate(input_image, size=(256, 256)) + 1) / 2).clamp(0, 1))
 
     def gen_256_img_hairmask(self, input_image): 
-        # image_transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])]) 
-        # input_image = image_transform(input_image).unsqueeze(0).cuda()
         input_seg = torch.argmax(self.seg(input_image)[0].clone().detach(), dim=1).long()
         input_hairmask = torch.where((input_seg == 10), torch.ones_like(input_seg), torch.zeros_like(input_seg))
         input_hairmask_256 = F.interpolate(input_hairmask.unsqueeze(0).float(), size=(256, 256))
@@ -47,12 +50,12 @@ class RefineProxy(torch.nn.Module):
         return input_img_256, input_hairmask_256
     
     def gen_256_img_facemask(self, input_image):
-        # image_transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])]) 
-        # input_image = image_transform(input_image).unsqueeze(0).cuda()
         input_seg = torch.argmax(self.seg(input_image)[0].clone().detach(), dim=1).long()
         input_hairmask = torch.where((input_seg == 10), torch.ones_like(input_seg), torch.zeros_like(input_seg))
         input_earmask = torch.where((input_seg == 6), torch.ones_like(input_seg), torch.zeros_like(input_seg))
-        hair_ear_mask = input_hairmask + input_earmask
+        input_bgmask = torch.where((input_seg == 0), torch.ones_like(input_seg), torch.zeros_like(input_seg))
+        input_clothes_mask = torch.where((input_seg == 15), torch.ones_like(input_seg), torch.zeros_like(input_seg))
+        hair_ear_mask = input_hairmask + input_earmask + input_bgmask + input_clothes_mask
         input_face_256 = 1 - (F.interpolate(hair_ear_mask.unsqueeze(0).float(), size=(256, 256)))
         input_img_256 = F.interpolate(input_image, size=(256, 256))
         return input_img_256, input_face_256
@@ -61,6 +64,7 @@ class RefineProxy(torch.nn.Module):
         image_transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])]) 
         ref_img = image_transform(ref_img).unsqueeze(0).cuda()
         ref_img_256, ref_hairmask_256 = self.gen_256_img_hairmask(ref_img)
+        _, source_hairmask_256 = self.gen_256_img_hairmask(src_image)
         source_img_256, source_facemask_256 = self.gen_256_img_facemask(src_image)
         blended_latent=blended_latent.requires_grad_(True)
         optimizer = torch.optim.Adam([blended_latent], lr=self.opts.lr_refine)
@@ -75,19 +79,24 @@ class RefineProxy(torch.nn.Module):
             optimizer.zero_grad()
             latent_in = torch.cat([blended_latent[:, :m_style, :], latent_end], dim=1)
             img_gen = self.generator.decoder.synthesis(latent_in, noise_mode='const')
+            img_gen_256 = F.interpolate(img_gen, size=(256, 256))
             #Hair loss
-            img_gen_256_hair, gen_hairmask_256 = self.gen_256_img_hairmask(img_gen)
-            hair_style_loss = self.transfer_loss_builder.style_loss(ref_img_256, img_gen_256_hair, mask1=ref_hairmask_256, mask2=gen_hairmask_256)
+            # img_gen_256_hair, gen_hairmask_256 = self.gen_256_img_hairmask(img_gen)
+            # hair_style_loss = self.transfer_loss_builder.style_loss(ref_img_256, img_gen_256_hair, mask1=ref_hairmask_256, mask2=gen_hairmask_256)
+            hair_loss = self.percept_with_mask(img_gen_256, ref_img_256,mask=ref_hairmask_256)
             #Face loss
-            img_gen_256_face, gen_facemask_256 = self.gen_256_img_facemask(img_gen)
-            face_loss = self.transfer_loss_builder.style_loss(source_img_256, img_gen_256_face, mask1=source_facemask_256, mask2=gen_facemask_256)
+            # img_gen_256_face, gen_facemask_256 = self.gen_256_img_facemask(img_gen)
+            # face_loss = self.transfer_loss_builder.style_loss(source_img_256, img_gen_256_face, mask1=source_facemask_256, mask2=gen_facemask_256)
+            no_hair_region = (1 - ref_hairmask_256) * (1 - source_hairmask_256)
+            face_loss = self.percept_with_mask(img_gen_256, source_img_256,mask=no_hair_region)
 
             delta_w_loss = self.delta_loss(blended_latent[:, :m_style, :], latent_prev)
 
-            gen_kp = self.inference_on_kp_extractor(img_gen)
-            kp_loss = self.landmark_loss(src_kp[:, :], gen_kp[:, :])
+            # gen_kp = self.inference_on_kp_extractor(img_gen)
+            # kp_loss = self.landmark_loss(src_kp[:, :], gen_kp[:, :])
 
-            loss = self.opts.style_lambda_ref * (hair_style_loss + face_loss) + self.opts.delta_w_lambda_ref * delta_w_loss + self.opts.landmark_lambda_ref * kp_loss
+            #loss = self.opts.style_lambda_ref * (hair_loss + face_loss) + self.opts.delta_w_lambda_ref * delta_w_loss + self.opts.landmark_lambda_ref * kp_loss
+            loss=hair_loss+face_loss
             
             latent_prev = blended_latent[:, :m_style, :].clone().detach()
             loss.backward()
